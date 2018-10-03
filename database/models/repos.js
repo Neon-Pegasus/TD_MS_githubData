ee3a1cbd229466415d904fc53d526e04e3d5706a
JBen13 <jessicabenjaminxiii@gmail.com>
2018-10-03 11:44:31 -0700

    Add model folder to the database and add complete Repo model

---
+++ database/models/repos.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const db = require('../db.js');
+
+
+
+const Repo = db.define('repo', {
+  repositoryName: Sequelize.STRING,
+  description: Sequelize.STRING,
+  stargazers: Sequelize.INTEGER,
+  pullRequestTitle: Sequelize.STRING,
+  pullRequestBody: Sequelize.STRING,
+  reviewsState: Sequelize.STRING,
+  reviewsBody: Sequelize.STRING,
+  commentsBody: Sequelize.STRING,
+  updatedAt: Sequelize.DATE,
+});
+
+
+module.exports = Repo;
---
+++ database/models/user.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const db = require('../db.js');
+
+const User = db.define('user' {
+
+});
+
+
+
+module.exports = User;
