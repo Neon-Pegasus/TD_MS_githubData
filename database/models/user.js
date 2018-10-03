f08cba10cef650b7795a38be48b10ac6a7ff9bcb
JBen13 <jessicabenjaminxiii@gmail.com>
2018-10-03 11:53:58 -0700

    Add user model and includes notes about the model.

--- database/models/user.js
+++ database/models/user.js
@@ -2,9 +2,18 @@ const Sequelize = require('sequelize');
 const db = require('../db.js');
 
 const User = db.define('user' {
-
+  gh_userName: Sequelize.STRING,
+  full_Name: Sequelize.STRING, 
+  pullRequestTitle: Sequelize.STRING,
+  pullRequestBody: Sequelize.STRING,
+  reviewsBody: Sequelize.STRING,
+  commentsBody: Sequelize.STRING,
 });
 
-
-
 module.exports = User;
+
+/**
+ * Model represents a table in the datbase. 
+ * full_Name is requested and given by github.
+ * gh_username will be given by the user directly. 
+ */
